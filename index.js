cffa950fb509eeb3e8ecea7670178b28a91ceb1b
kifeb <rizkyfebrianto947@gmail.com>
2022-12-01 10:21:23 +0700

    make it better

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const db = require("./config/database");
 const usersRoutes = require("./routes/users")
+const multer = require("multer");
 
 const app = express();
 const port = process.env.PORT || 5000
@@ -9,11 +10,28 @@ const port = process.env.PORT || 5000
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-db.sync({force: true}).then(() => {
+db.sync({force: false}).then(() => {
     console.log("Database Connected");
 }).catch((error) => {
     console.error("Failed to sync database : " + error.message);
     process.exit(1);
+})
+
+//Upload foto
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+	  cb(null, "public/upload");
+	},
+	filename: function (req, file, cb) {
+	  cb(null, Date.now() + file.originalname);
+	},
+  });
+  
+  const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+   const file = req.file;
+   res.status(200).json(file.filename)
 })
 
 app.get("/", (req, res) => {
